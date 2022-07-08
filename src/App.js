4aa7c333f2b75ea33c753809f7b14f302aeefced
praveen-71 <105632325+praveen-71@users.noreply.github.com>
2022-07-08 16:49:07 +0530

    a

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import Register from './Register';
+import Login from './Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Cards from './Cards';
+import { useState } from 'react';
+// import Dashboard from './Dashboard';
+import Menu from './Menu';
+import Navbar from './Navbar';
+import Userdetails from './Userdetails';
+import Dash from './Dash';
+import Piechart from './Piechart';
+import Logout from './Logout';
+
+
+
+function App() {
+  const[userid,setuserid] = useState()
+  return (
+    <BrowserRouter>
+    <div className="App">
+        <Navbar/>
+       <Routes>
+       {/* <Route path='/' element={<Register/>}></Route> */}
+       <Route path='/login' element={<Login setuserid={setuserid}/>}></Route>
+       <Route path='/dashboard' element={<Dash/>}></Route>
+       <Route path='/user' element={<Userdetails/>}></Route>
+       <Route path='/chart' element={<Piechart/>}></Route>
+       <Route path='/logout' element={<Logout/>}></Route>
+
+
+       {/* 
+      <Route path='/card' element={<Cards userid={userid}/>}></Route> */}
+
+        </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
