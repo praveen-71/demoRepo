10c1feb63de602006fa58f9e289e327b9fc7b2f7
praveen-71 <105632325+praveen-71@users.noreply.github.com>
2022-07-08 18:04:56 +0530

    ch1

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ function App() {
         <Navbar/>
        <Routes>
        {/* <Route path='/' element={<Register/>}></Route> */}
-       <Route path='/login' element={<Login setuserid={setuserid}/>}></Route>
+       {/* <Route path='/login' element={<Login setuserid={setuserid}/>}></Route>
        <Route path='/dashboard' element={<Dash/>}></Route>
        <Route path='/user' element={<Userdetails/>}></Route>
        <Route path='/chart' element={<Piechart/>}></Route>
-       <Route path='/logout' element={<Logout/>}></Route>
+       <Route path='/logout' element={<Logout/>}></Route> */}
 
 
        {/* 
